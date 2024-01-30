2dc1b715dc6629885571df10ea8620f0dc75711a
Jenny Shen <jenshen@pennmedicine.upenn.edu>
2024-01-30 16:13:46 -0500

    modified genDat function details

--- R/genDat.R
+++ R/genDat.R
@@ -23,19 +23,19 @@ genDat <- function(seedNum, p11, p10, valType, n=NULL, alphaInt= NULL){
   
   p <- 5
   
-  x.A <- matrix( rnorm(n*(p),0,1),n,(p))
-  pt.A <- expit(0.8+0.3*rowSums(x.A)) 
+  x <- matrix( rnorm(n*(p),0,1),n,(p))
+  ptrt <- expit(0.8+0.3*rowSums(x)) 
   
-  t.A <- rbinom(n, 1, pt.A)
-  py.A <- expit(-3.9 + t.A+rowSums(x.A))
+  trt <- rbinom(n, 1, ptrt)
+  py.A <- expit(-3.9 + trt+rowSums(x))
   
   y <- rbinom(n, 1, py.A) 
   
   
-  partDat = data.frame(t.A, x.A)
-  estAlpha <- glm(t.A ~ . , data=partDat, family="binomial")
+  partDat = data.frame(trt, x)
+  estAlpha <- glm(trt ~ . , data=partDat, family="binomial")
   estAlpha_coef <- coef(estAlpha)
-  estE <- expit(cbind(1,x.A)%*%estAlpha_coef)
+  estE <- expit(cbind(1,x)%*%estAlpha_coef)
   
   ## generate outcomes subject to misclassification
   yStar <- induceME(y, p11=p11, p10=p10)
@@ -50,14 +50,14 @@ genDat <- function(seedNum, p11, p10, valType, n=NULL, alphaInt= NULL){
     inVal <- rbinom(n, size=1, prob=0.17)
   } else{
     alpha0 <- c(alphaInt, 0.5, 1, 1, 1, 1, rep(0, p-4))
-    probB <- (1+exp(-cbind(1,t.A, x.A)%*%alpha0))^(-1)
+    probB <- (1+exp(-cbind(1,trt, x)%*%alpha0))^(-1)
     inVal <- rbinom(n,size = 1,prob = (probB) )
   }
   
   B.loc <- which(inVal == 1)
   
   
-  allDat = data.frame(y.Both, inVal, t.A, x.A)
+  allDat = data.frame(y.Both, inVal, trt, x)
   
   return(allDat)
   
